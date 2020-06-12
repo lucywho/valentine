bc473485d105accb93254dfc1a1ac77edadba60f
Lucy Toman <lucy@toman.me.uk>
2020-06-12 11:28:10 +0200

    removes case sensitivity, improves styling

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function checkName(name) {
-  let nameIn = document.getElementById(name).value;
-  if (document.getElementById(name).value === "Tim") {
-    alert("Roses are red, Violets are blue, I love " + nameIn + " and he loves me too!");
-  } else {
-    alert("I'm sorry, " + nameIn + ", but your wife didn't make you a website for Valentine's Day.");
-  };
+    let nameIn = document.getElementById(name).value;
+    nameCheck = nameIn.toUpperCase();
+    if (nameCheck === "TIM") {
+        alert(
+            "Roses are red, Violets are blue, I love Tim and he loves me too!"
+        );
+    } else if (nameCheck === "") {
+        alert("Please enter your name!");
+    } else {
+        alert(
+            "I'm sorry, " +
+                nameIn +
+                ", but your wife didn't make you a website for Valentine's Day."
+        );
+    }
 }
